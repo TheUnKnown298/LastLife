e1b037259ca529b40fa6664df797051665db0830
TheUnKnown298 <47735851+TheUnKnown298@users.noreply.github.com>
2023-08-22 23:36:14 +0700

    Basic user movement and adjust player speed
    Basic user movement and adjust player speed

--- Assets/Scripts/GeneralScript.cs
+++ Assets/Scripts/GeneralScript.cs
@@ -7,6 +7,7 @@ public class GeneralScript : MonoBehaviour
     private bool fingerDown;
     private Vector2 startPosition;
     public Player player;
+    private Vector2 moveDirection;
 
     void Start()
     {
@@ -15,22 +16,25 @@ public class GeneralScript : MonoBehaviour
 
     void Update()
     {
-        if (fingerDown == false && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
-        {
-            fingerDown = true;
-            startPosition = Input.touches[0].position;
-        }
+        // if (fingerDown == false && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        // {
+        //     fingerDown = true;
+        //     startPosition = Input.touches[0].position;
+        // }
 
-        if (fingerDown)
-        {
-            Debug.Log(Vector2.Angle(Input.touches[0].position, startPosition));
-            player.Move(new Vector2(Input.touches[0].position.x - startPosition.x, Input.touches[0].position.y - startPosition.y));
-            if (Input.GetMouseButtonUp(0))
-            {
-                Debug.Log("lasdlasldasld");
-                fingerDown = false;
-                player.Stop(new Vector2(0, 0));
-            }
-        }
+        // if (fingerDown)
+        // {
+        //     // Debug.Log(Vector2.Angle(Input.touches[0].position, startPosition));
+        //     // Debug.Log(Input.GetAxisRaw("Vertical"));
+        //     // player.Move(new Vector2(Input.touches[0].position.x - startPosition.x, Input.touches[0].position.y - startPosition.y));
+        //     // player.Move(new Vector2(Input.touches[0].position.x, Input.touches[0].position.y));
+        //     // player.Move(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized);
+
+        //     if (Input.GetMouseButtonUp(0))
+        //     {
+        //         fingerDown = false;
+        //         player.StopMoving(new Vector2(0, 0));
+        //     }
+        // }
     }
 }
--- Assets/Scripts/MCScript.cs
+++ Assets/Scripts/MCScript.cs
@@ -6,30 +6,58 @@ public class Player : MonoBehaviour
 
 
 {
-    private Rigidbody2D playerBody;
+    public Rigidbody2D playerBody;
     public Player player;
     public float moveSpeed;
-    private Vector3 targetLocation;
+    private Vector2 targetLocation;
+    private Vector2 moveDirection;
 
     void Start()
     {
         playerBody = GetComponent<Rigidbody2D>();
         targetLocation = transform.position;
-        moveSpeed = 5;
+        moveSpeed = 7;
     }
 
     void Update()
     {
+        ProcessInput();
         transform.position = Vector3.MoveTowards(transform.position, targetLocation, moveSpeed * Time.deltaTime);
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Stop();
+        }
     }
 
-    public void Move(Vector3 moveDirection)
-    {
-        targetLocation += moveDirection;
-    }
-
-    public void Stop(Vector3 moveDirection)
+    public void Stop()
     {
         targetLocation = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        GetPlayerTargetLocation();
+    }
+
+    void ProcessInput()
+    {
+
+
+        float moveX = Input.GetAxis("Mouse X");
+        float moveY = Input.GetAxis("Mouse Y");
+
+        if (Input.touchCount > 0)
+        {
+            moveX = Input.touches[0].deltaPosition.x;
+            moveY = Input.touches[0].deltaPosition.y;
+        }
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void GetPlayerTargetLocation()
+    {
+        targetLocation += new Vector2(moveDirection.x, moveDirection.y);
     }
 }
